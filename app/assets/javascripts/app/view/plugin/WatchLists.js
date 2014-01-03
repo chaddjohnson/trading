dc2b5bdb20ece7a10e01036f19f14acad772baf7
Chad Johnson <chad.d.johnson@gmail.com>
2014-01-03 16:44:17 -0600

    Added high-cap stocks as defaults to watch list, and removed excess watch lists.

--- app/assets/javascripts/app/view/plugin/WatchLists.js
+++ app/assets/javascripts/app/view/plugin/WatchLists.js
@@ -20,23 +20,24 @@ Ext.define('Trading.view.plugin.WatchLists', {
     this.add([
       {
         xtype: 'watch-list',
-        title: 'Losers',
+        title: 'High-Cap',
         //symbols: ['AMZN','VIPS','FB','AAPL','OAS'],
-        symbols: ['AMZN','VIPS','FB','AAPL','OAS','HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR','CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
-        quoteServiceClient: this.quoteServiceClient
-      },{
-        xtype: 'watch-list',
-        title: 'Day Trading',
-        //symbols: ['HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR'],
-        symbols: ['AMZN','VIPS','FB','AAPL','OAS','HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR','CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
-        quoteServiceClient: this.quoteServiceClient
-      },{
-        xtype: 'watch-list',
-        title: 'Indexes',
-        //symbols: ['CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
-        symbols: ['AMZN','VIPS','FB','AAPL','OAS','HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR','CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
+        symbols: ['GOOG','FB','TWTR','AMZN','V','XOM','AAPL','INTC','CSCO','PCLN','GE'],
         quoteServiceClient: this.quoteServiceClient
       }
+      // ,{
+      //   xtype: 'watch-list',
+      //   title: 'Day Trading',
+      //   //symbols: ['HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR'],
+      //   symbols: ['AMZN','VIPS','FB','AAPL','OAS','HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR','CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
+      //   quoteServiceClient: this.quoteServiceClient
+      // },{
+      //   xtype: 'watch-list',
+      //   title: 'Indexes',
+      //   //symbols: ['CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
+      //   symbols: ['AMZN','VIPS','FB','AAPL','OAS','HTCH','HXM','EXPE','SWI','TSYS','ZNGA','OUTR','CRUS','TPX','BAS','NTGR','DECK','SMTC','TGI','VRTX','DLR','EVC','SLT','MTSN','HSOL'],
+      //   quoteServiceClient: this.quoteServiceClient
+      // }
     ]);
   }
 });
